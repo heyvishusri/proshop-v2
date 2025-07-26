d9e227e5ca8c10d97e8dea46112bc7a76cbc5f6f
heyvishusri <vishwaskumar55347@gmail.com>
2025-07-27 02:40:48 +0530

    Header, product Updated

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -3,26 +3,40 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 
 function CollapsibleExample() {
   return (
-    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-      <Container bg="">
-        <Navbar.Brand href="#home" className="text-white">
-          <img src={logo} alt="ProShop" />
-          ProShop
-        </Navbar.Brand>
+    <Navbar
+      collapseOnSelect
+      expand="md"
+      bg="dark"
+      variant="dark"
+      sticky="top"
+      style={{ opacity: 0.8 }}
+    >
+      <Container>
+        <LinkContainer to="/">
+          <Navbar.Brand className="text-white">
+            <img src={logo} alt="ProShop" />
+            ProShop
+          </Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto"></Nav>
           <Nav>
-            <Nav.Link href="/cart">
-              <FaShoppingCart /> Cart
-            </Nav.Link>
-            <Nav.Link eventKey={2} href="/signin">
-              <FaUser /> Sign In
-            </Nav.Link>
+            <LinkContainer to="/cart">
+              <Nav.Link>
+                <FaShoppingCart /> Cart
+              </Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link eventKey={2}>
+                <FaUser /> Sign In
+              </Nav.Link>
+            </LinkContainer>
           </Nav>
         </Navbar.Collapse>
       </Container>
