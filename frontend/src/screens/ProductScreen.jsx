dedb828e712018e64b8d09e40fc32a277428ea6c
heyvishusri <vishwaskumar55347@gmail.com>
2025-07-27 04:02:39 +0530

    Product Screen Updated

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Card,
+  Button,
+  ListGroupItem,
+} from "react-bootstrap";
+import products from "../products";
+import Rating from "../components/Rating";
+const ProductScreen = () => {
+  const { id: productId } = useParams();
+  const product = products.find((p) => p._id === productId);
+  console.log(product);
+  return (
+    <>
+      <Link className="btn btn-light my-3 " to="/">
+        {" "}
+        Go Back
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} Reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ₹{product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}></Col>
+      </Row>
+    </>
+  );
+};
+
+export default ProductScreen;
