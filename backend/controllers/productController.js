490cbc8d643e3c51ec4617b4173b5375aadd897a
heyvishusri <vishwaskumar55347@gmail.com>
2025-08-15 01:15:13 +0530

    updated controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,25 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+//@Fetch all products
+//Route: GET /api/products
+//Access: Public
+export const getProduct = asyncHandler(async (req, res) => {
+  const products = await Product.find({});
+
+  res.json(products);
+});
+
+//@Fetch product by ID
+//Route: GET /api/products/:id
+//Access: Public
+export const getProductById = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    return res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
+  }
+});
