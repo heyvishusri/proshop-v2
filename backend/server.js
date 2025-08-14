020a64842f72d5934b1c852f3519a82b63ac5719
heyvishusri <vishwaskumar55347@gmail.com>
2025-08-14 23:05:06 +0530

    create server

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+
+import productRoutes from "./routes/productRoutes.js";
 
 const port = process.env.PORT || 5000;
 
@@ -14,18 +15,6 @@ app.get("/", (req, res) => {
   res.send("API is running....");
 });
 
-app.get("/api/products", (req, res) => {
-  try {
-    res.json(products);
-  } catch (error) {
-    console.error("Server error:", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
