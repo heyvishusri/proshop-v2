a8e3318f05ed9084c8bb0be3a43ec2777bbb7fda
heyvishusri <vishwaskumar55347@gmail.com>
2025-08-18 23:55:20 +0530

    store apis updated

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { apiSlice } from "./slices/apiSlice";
 
 const store = configureStore({
   reducer: {
     // Add your reducers here
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true, // Enable Redux DevTools
 });
 export default store;
