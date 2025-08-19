236cfe782682ab88524f0c90e8eccf68973035b0
heyvishusri <vishwaskumar55347@gmail.com>
2025-08-19 18:01:34 +0530

    implement logic for cart

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -6,6 +6,10 @@ const initialState = localStorage.getItem("cart")
       cartItems: [],
     };
 
+const addDecimals = (num) => {
+  return (Math.round(num * 100) / 100).toFixed(2);
+};
+
 const cartSlice = createSlice({
   name: "cart",
   initialState,
@@ -21,12 +25,26 @@ const cartSlice = createSlice({
         state.cartItems = [...state.cartItems, item];
       }
       //Calculate item price
-
-      //Calculate shipping price
-      //Calculate tax price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+      //Calculate shipping price(if order is more than Rs.1000, shipping is free else Rs.10)
+      state.shippingPrice = addDecimals(state.itemsPrice > 1000 ? 0 : 10);
+      //Calculate tax price (5%)
+      state.taxPrice = addDecimals(
+        Number((0.05 * state.itemsPrice).toFixed(2))
+      );
       //Calculate total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+      //Save cart to localStorage
+      localStorage.setItem("cart", JSON.stringify(state));
     },
   },
 });
 
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
