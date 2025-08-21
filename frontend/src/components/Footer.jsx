c3115d7915365eee7cc35d684b82dddd88b119fc
heyvishusri <vishwaskumar55347@gmail.com>
2025-08-21 23:15:05 +0530

    updated Footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
-const Footer1 = () => {
+const Footer = () => {
   const currentYear = new Date().getFullYear();
   return (
     <footer>
