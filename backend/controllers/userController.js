bcaf66d1be147a9b6e3c51ff723b12fe41518e42
heyvishusri <vishwaskumar55347@gmail.com>
2025-11-10 23:35:14 +0530

    jwt only cookies

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken";
 
 //@desc auth user & get token
 //Route: Post /api/users/login
@@ -9,11 +10,22 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d"
+    });
+
+    //set JWT as HTTP-Only cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV !== "development",
+      sameSite: "strict",
+      maxAge: 30 * 24 * 60 * 60 * 1000 // 30 days
+    });
     res.json({
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin,
+      isAdmin: user.isAdmin
     });
   } else {
     res.status(401);
@@ -80,5 +92,5 @@ export {
   getUsers,
   getUserById,
   deleteUser,
-  updateUser,
+  updateUser
 };
