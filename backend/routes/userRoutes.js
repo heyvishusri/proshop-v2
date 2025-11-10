eebb32a0385214b477a677bd8c8868767bc777d6
heyvishusri <vishwaskumar55347@gmail.com>
2025-11-10 23:56:06 +0530

    authmiddleware & endpoints

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -9,15 +9,19 @@ import {
   getUsers,
   getUserById,
   deleteUser,
-  updateUser,
+  updateUser
 } from "../controllers/userController.js";
 
+import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 router.route("/").post(registerUser).get(getUsers);
 router.post("/login", authUser);
 router.post("/logout", logoutUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 router.route("/:id").get(getUserById).delete(deleteUser).put(updateUser);
 
 // Export the router to be used in the main app
